d886879d77de50f381bb0c3be6d890763a2deb14
金灵波 <492617233@qq.com>
2019-12-13 14:14:53 +0800

    [ADD] 实现卡片翻转效果动画

--- lib/src/pages/Course/find_picture_template/find_picture_card_widget.dart
+++ lib/src/pages/Course/find_picture_template/find_picture_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flip_card/flip_card.dart';
 
 class FindPictureCardWidget extends StatefulWidget {
   @override
@@ -7,13 +8,51 @@ class FindPictureCardWidget extends StatefulWidget {
 }
 
 class _FindPictureCardWidget extends State<FindPictureCardWidget> {
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    startAnimation();
+  }
+
+  startAnimation() {
+    Future.delayed(Duration(milliseconds: 300), () {
+      cardKey.currentState.toggleCard();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      height: 150,
-//      decoration: BoxDecoration(color: Colors.blue),
-      child: Image.asset("images/find_picture_card_back_green.png",fit: BoxFit.fill,),
-    );
+    return FlipCard(
+        key: cardKey,
+        flipOnTouch: false,
+        direction: FlipDirection.HORIZONTAL,
+        front: Container(
+          width: 150,
+          height: 150,
+          child: Image.asset(
+            "images/find_picture_card_back_green.png",
+            fit: BoxFit.fill,
+          ),
+        ),
+        back: Container(
+            width: 150,
+            height: 150,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Image.asset(
+                  "images/find_picture_card_green.png",
+                  fit: BoxFit.fill,
+                ),
+                Image.asset(
+                  "images/judgement_graph_weight_3.png",
+                  fit: BoxFit.fill,
+                ),
+                Text("I am tiger")
+              ],
+            )));
   }
 }
