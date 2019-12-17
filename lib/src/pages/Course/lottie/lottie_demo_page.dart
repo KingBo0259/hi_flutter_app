4ce7fb1012d28e97962a8f2dcc6519cabd31bd18
金灵波 <492617233@qq.com>
2019-12-17 10:48:51 +0800

    [ADD] 添加说明

--- lib/src/pages/Course/lottie/lottie_demo_page.dart
+++ lib/src/pages/Course/lottie/lottie_demo_page.dart
@@ -12,18 +12,24 @@ class _LottieDemoPage extends State<LottieDemoPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          middle: Text("Lottie Demo"),
-        ),
-        child: Center(
-          child: Container(
-              width: 200,
-              height: 200,
-              child: LottieView.fromFile(
-                  autoPlay: true,
-                  loop: true,
-                  onViewCreated: (vc) {},
-                  filePath: "json/lottie/compare_weight_lighter_1.json")),
-        ));
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Lottie Demo"),
+      ),
+      child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+               Text("1、单独使用Lottie plugin 目前好像无法解决使用带有图片的json文件.\n2\需要我们自己去修改和扩展;\n 3.如果带有图片可以使用本地资源去解决") ,
+            Container(
+                width: 200,
+                height: 200,
+                child: LottieView.fromFile(
+                    autoPlay: true,
+                    loop: true,
+                    onViewCreated: (vc) {},
+                    filePath: "json/lottie/compare_weight_lighter_1.json")),
+          ])),
+    );
   }
 }
